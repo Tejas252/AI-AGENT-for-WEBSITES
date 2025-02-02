f162ee2939fd2026a3b5960b51edb1f24df0a4f6
Tejas <tejas.scaleteam@gmail.com>
2025-02-02 23:59:36 +0530

    Revert "set out dir for ts"
    This reverts commit b816990269d4e52bd772b60b3464c220101ef35c.

--- dist/services/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createEmbedding = void 0;
-var index_1 = require("./gemini/index");
-Object.defineProperty(exports, "createEmbedding", { enumerable: true, get: function () { return index_1.createEmbedding; } });
--- dist/utils/db.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const chromadb_1 = require("chromadb");
-const client = new chromadb_1.ChromaClient();
-client.heartbeat().then(console.log);
-exports.default = client;
